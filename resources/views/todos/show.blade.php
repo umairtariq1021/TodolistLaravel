50b5893c0637f7a46c4ed58e432d69c84b5483c5
unknown <umairtariq1021@gmail.com>
2019-11-09 12:53:13 +0500

    todolistlaravel project

---
+++ resources/views/todos/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+<a href="/" class="btn btn-primary">Go Back</a>
+<h1> {{$todo->text}}</h1>
+<div class="badge badge-pill badge-danger">{{$todo->due}}</div>
+<hr>
+<p>{{$todo->body}}</p>
+<br><br>
+<a href="/todo/{{$todo->id}}/edit" class="btn btn-warning">Edit</a>
+{!! Form::open(['action' => ['TodosController@destroy', $todo->id], 'method'=>'POST', 'class' => 'pull-right' ])!!}
+         {{Form::hidden('_method','DELETE')}}
+      {{ Form::bsSubmit('Delete', ['class'=>'btn btn-danger']) }}
+{!! Form::close() !!}
+
+
+@endsection
